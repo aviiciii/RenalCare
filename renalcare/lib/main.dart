b2530fed1cb1cc3f91081ca835843d467a54d6d5
aviiciii <laavesh1@gmail.com>
2024-02-28 20:06:52 +0530

    Firebase Setup

--- renalcare/lib/main.dart
+++ renalcare/lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() {
+// firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
