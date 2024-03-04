46e7f2ee59d3d54daeb546b9f48dfc06b043d8f6
Harrish <harrishoffical@gmail.com>
2024-03-04 13:21:26 +0530

    onboarding started

--- renalcare/lib/main.dart
+++ renalcare/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 // firebase
 import 'package:firebase_core/firebase_core.dart';
+import 'package:renalcare/pages/Onbarding_user_detail1';
 import 'package:renalcare/pages/splashScreen.dart';
 import 'firebase_options.dart';
 
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const splashScreen(),
+      home: const OnboardingUserDetail1() as Widget?,
     );
   }
 }
