4fbb379d35d4a3c3e64aafd5a650cab2fb77a32e
aviiciii <laavesh1@gmail.com>
2024-03-05 23:15:47 +0530

    added input page

--- renalcare/lib/input.dart
+++ renalcare/lib/input.dart
@@ -3,6 +3,34 @@ import 'package:flutter/material.dart';
 class TextFieldPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Text Fields'),
+      ),
+      body:const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Input Field 1',
+              ),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Input Field 2',
+              ),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Input Field 3',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
