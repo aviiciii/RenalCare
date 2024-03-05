47f93bda79f33faad873bd91554e3f236f94ad38
itzkrizh <itzkrizh@gmail.com>
2024-03-06 00:27:53 +0530

    Bottom Navigator

--- renalcare/lib/pages/homepage.dart
+++ renalcare/lib/pages/homepage.dart
@@ -1,37 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:renalcare/widgets/bottomnav.dart';
 
 class homePage extends StatelessWidget {
   const homePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.blue,
-        type: BottomNavigationBarType.fixed,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Discover',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_a_photo),
-            label: 'Add Today\'s Diet',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.cyclone_sharp),
-            label: 'Dialysis cycle',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-      ),
+    return const Scaffold(
+      bottomNavigationBar: bottomNav() ,
     );
   }
 }
